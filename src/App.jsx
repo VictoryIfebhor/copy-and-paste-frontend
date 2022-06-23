8ae3fb7cf3a26e9a28e7f552f5939c831065270e
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-06-23 14:20:28 +0100

    Made the route page protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useGlobalContext } from "./context";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 const App = () => {
+  const { user } = useGlobalContext();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
+          <Route index element={user ? <Main /> : <Navigate to={"/auth"} />} />
           <Route path="auth" element={<Login />} />
         </Route>
       </Routes>
