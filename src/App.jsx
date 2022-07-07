ea3ca6eab03348646a0e9e47e0d79275e6e05fdc
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-07-07 16:31:43 +0100

    changed the code to check for token instead of user before rendering protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useGlobalContext } from "./context";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 const App = () => {
-  const { user } = useGlobalContext();
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={user ? <Main /> : <Navigate to={"/auth"} />} />
+        <Route
+          index
+          element={
+            localStorage.getItem("jwt") ? <Main /> : <Navigate to={"/auth"} />
+          }
+        />
         <Route path="auth" element={<Login />} />
         <Route path="*" element={<Navigate to={"/auth"} />} />
       </Route>
